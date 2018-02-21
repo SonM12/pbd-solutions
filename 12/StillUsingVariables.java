e7e935866f52255bce5c3a561b9d98e9c0f3ac8c
SonM12 <31938244+SonM12@users.noreply.github.com>
2018-02-21 09:20:40 -0500

    Commit programming by doing folder 12

---
+++ 12/StillUsingVariables.java
@@ -0,0 +1,8 @@
+public class StillUsingVariables {
+    public static void main (String[] args){
+        String name = "Roy Son";
+        int year = 2018;
+        
+        System.out.println("My name is " + name + " and I'll graduate in " + year + ".");
+    }
+} 
